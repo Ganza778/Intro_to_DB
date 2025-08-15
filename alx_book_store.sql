91a27718e5133bbf46fba7cfde8a45236f4d375a
Aristide <gnaristide@gmail.com>
2025-08-15 18:13:15 +0200

    made changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,25 +22,25 @@ CREATE TABLE IF NOT EXISTS Books (
         ON UPDATE CASCADE
 );
 
--- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
-);
+) ENGINE=InnoDB;
 
--- Create Orders table
+
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
+    customer_id INT NOT NULL,
     order_date DATE,
     CONSTRAINT fk_customer
         FOREIGN KEY (customer_id)
         REFERENCES Customers(customer_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-);
+) ENGINE=InnoDB;
+
 
 -- Create Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details (
