44dadce50a4505d5f4989668ce2f84d4740f8302
Aristide <gnaristide@gmail.com>
2025-08-15 18:07:04 +0200

    made changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,27 +4,27 @@ USE alx_book_store;
 
 -- Create Authors table
 CREATE TABLE IF NOT EXISTS Authors (
-    authorID INT AUTO_INCREMENT PRIMARY KEY,
+    author-id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Create Books table
 CREATE TABLE IF NOT EXISTS Books (
-    bookID INT AUTO_INCREMENT PRIMARY KEY,
+    book-id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
-    authorID INT,
+    author-id INT,
     price DOUBLE,
     publication_date DATE,
     CONSTRAINT fk_author
-        FOREIGN KEY (authorID)
-        REFERENCES Authors(authorID)
+        FOREIGN KEY (author-id)
+        REFERENCES Authors(author-id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
 -- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
-    customerID INT AUTO_INCREMENT PRIMARY KEY,
+    customer-id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
@@ -32,30 +32,30 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 -- Create Orders table
 CREATE TABLE IF NOT EXISTS Orders (
-    orderID INT AUTO_INCREMENT PRIMARY KEY,
-    customerID INT,
+    order-id INT AUTO_INCREMENT PRIMARY KEY,
+    customer-id INT,
     order_date DATE,
     CONSTRAINT fk_customer
-        FOREIGN KEY (customerID)
-        REFERENCES Customers(customerID)
+        FOREIGN KEY (customer-id)
+        REFERENCES Customers(customer-id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 -- Create Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details (
-    orderdetailID INT AUTO_INCREMENT PRIMARY KEY,
-    orderID INT,
-    bookID INT,
+    orderdetail-id INT AUTO_INCREMENT PRIMARY KEY,
+    order-id INT,
+    book-id INT,
     quantity DOUBLE,
     CONSTRAINT fk_order
-        FOREIGN KEY (orderID)
-        REFERENCES Orders(orderID)
+        FOREIGN KEY (order-id)
+        REFERENCES Orders(order-id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     CONSTRAINT fk_book
-        FOREIGN KEY (bookID)
-        REFERENCES Books(bookID)
+        FOREIGN KEY (book-id)
+        REFERENCES Books(book-id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
