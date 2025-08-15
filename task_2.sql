defd50fcd6f52ba014e3506f008b8488cdb0343a
Aristide <gnaristide@gmail.com>
2025-08-15 19:26:36 +0200

    task 2 file

---
+++ task_2.sql
@@ -0,0 +1,38 @@
+USE alx_book_store;
+
+CREATE TABLE AUTHORS (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+) ENGINE=InnoDB;
+
+CREATE TABLE BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DECIMAL(10,2),
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE CUSTOMERS (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+) ENGINE=InnoDB;
+
+CREATE TABLE ORDERS (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE ORDER_DETAILS (
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+) ENGINE=InnoDB;
